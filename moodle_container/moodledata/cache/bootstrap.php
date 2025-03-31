e93dad96391a612bec6d165ea63cd3984491d14f
sherwin-ad <sherwin_ad@yahoo.com>
2025-03-31 19:16:58 +0800

    updated

---
+++ moodle_container/moodledata/cache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '9hBlv6Z9ngYuOMnhFNXTlOLD6TyE7A7ylocalhost';
+$CFG->bootstraphash = '03a3118df65f734c9addc4a59755ef95';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
